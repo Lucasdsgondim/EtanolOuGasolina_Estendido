28bb213c989263829251480d0a51a54bd8af243f
Lucas Gondim <lucasdsgondim@gmail.com>
2025-11-01 12:56:27 -0300

    Removed Unused Imports and Corrected var "eficiencia" mutable state type.

--- app/src/main/java/com/example/etanoougasolina_extendido/MainActivity.kt
+++ app/src/main/java/com/example/etanoougasolina_extendido/MainActivity.kt
@@ -10,32 +10,25 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.TextAutoSize
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.AdUnits
 import androidx.compose.material.icons.filled.Calculate
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.LocalGasStation
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationRail
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SegmentedButton
 import androidx.compose.material3.SegmentedButtonDefaults
 import androidx.compose.material3.SingleChoiceSegmentedButtonRow
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -45,7 +38,6 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -131,14 +123,13 @@ fun Calculadora(modifier: Modifier = Modifier) {
                     autoSize = TextAutoSize.StepBased(                    ),
                 )
                 BasicText(text = "Informe os preços de litro dos combustíveis e a distância entre os postos",
-                    style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Thin),
+                    style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Light),
                     modifier = Modifier.fillMaxWidth()
                         .weight(0.625f),
                     autoSize = TextAutoSize.StepBased())
                 Spacer(modifier = Modifier.weight(.1f))
                 BasicText(text = "Valor do Etanol",
-                    style = TextStyle(fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold),
+                    style = TextStyle(fontSize = 18.sp),
                     modifier = Modifier.fillMaxWidth()
                         .weight(0.325f),
                     autoSize = TextAutoSize.StepBased()
@@ -155,8 +146,7 @@ fun Calculadora(modifier: Modifier = Modifier) {
                 var valorgasolina by rememberSaveable { mutableStateOf("") }
                 Spacer(modifier = Modifier.weight(.1f))
                 BasicText(text = "Valor da Gasolina",
-                    style = TextStyle(fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold),
+                    style = TextStyle(fontSize = 18.sp),
                     modifier = Modifier.fillMaxWidth()
                         .weight(0.325f),
                     autoSize = TextAutoSize.StepBased())
@@ -170,12 +160,11 @@ fun Calculadora(modifier: Modifier = Modifier) {
                     prefix = { Text("R$") },
                     keyboardOptions = KeyboardOptions(keyboardType = androidx.compose.ui.text.input.KeyboardType.Number))
                 Spacer(modifier = Modifier.weight(.1f))
-                var eficiencia by rememberSaveable { mutableStateOf(0.7f) }
+                var eficiencia by rememberSaveable { mutableFloatStateOf(0.7f) }
                 val opts = listOf(0.7f, 0.75f)
                 BasicText(text = "Critério de eficiência",
                     style = TextStyle(
                         fontSize = 18.sp,
-                        fontWeight = FontWeight.Bold,
                         textAlign = TextAlign.Center),
                     modifier = Modifier.fillMaxWidth()
                         .weight(0.325f),
